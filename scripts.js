9d5ab13a2cde6424c5cf3ce4af597888dd8f6c03
Yashwanth Rami Reddy Gogireddy <g.yashwanth@op.iitg.ac.in>
2024-12-06 16:10:33 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Placeholder for JavaScript logic
+// Example for handling form submission
+
+document.getElementById('profileForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Profile updated successfully!');
+  });
+  
